052627258b42e3ca4cb21dbd801721e98767a2ee
habibyeo2 <habibyeo2@gmail.com>
2020-08-10 14:15:44 -0400

    first commit

---
+++ javaFiles/landingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home =() =>{
+
+    return(
+        <div className='container home'>
+            <h4 className="center">home</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Officiis, consectetur!</p>
+        </div>
+    )
+}
+
+export default Home
